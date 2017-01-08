d96fd9f722fb02ce7856c940382301aa33263825
Grayden Husband <graydenh@gmail.com>
2017-01-09 08:42:14 +1100

    add message to browser UI

--- sockets.js
+++ sockets.js
@@ -13,9 +13,9 @@ io.on("connection", function (socket){
 		console.log("Message received..." + message.text);
 
 		//io.emit sends to everbody
-
+		io.emit("message", message);
 		// sends to everybody except the sender
-		socket.broadcast.emit("message", message);
+		// socket.broadcast.emit("message", message);
 	})
 
 	socket.emit("message", {
